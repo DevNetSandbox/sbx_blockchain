b424ac35829cc2674b49e0f0bf5880677f840e55
iceworld <gosoumei@hotmail.com>
2017-08-01 21:49:24 +0800

    Update setupPbft.sh

--- 0.6/setupPbft.sh
+++ 0.6/setupPbft.sh
@@ -15,6 +15,6 @@ docker pull yeasy/hyperledger-fabric:0.6-dp \
   && docker tag yeasy/hyperledger-fabric-base:0.6-dp hyperledger/fabric-baseimage \
   && docker tag yeasy/hyperledger-fabric:0.6-dp hyperledger/fabric-membersrvc
 
-cd pbft && docker-compose -f 4-peers.yml up
+cd pbft && docker-compose -f 4-peers-with-explorer.yml up
 
 #test: curl HOST:5000/network/peers
